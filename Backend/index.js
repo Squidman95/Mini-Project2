e1937b6300cbc6758486903b47fa0272554343d2
GodOfAllBeings <s174871@student.dtu.dk>
2022-05-20 13:59:39 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -135,11 +135,11 @@ app.put('/customers/:customerId/basket/:productId', (req,res) => {  // Essential
 
 app.post('/customers/:customerId/basket', (req,res) => {       // Essential
     let basket = basketUtils.createBasket(req.params.customerId);
-    if(basket === null || basket === undefined) {
-        res.status(404).send('Customer does not exists');
-    } else {
+    // if(basket === null || basket === undefined) {
+    //     res.status(404).send('Customer does not exists');
+    // } else {
         res.status(200).send(basket);
-    }
+    // }
 });
 
 app.delete('/customers/:customerId/basket/:productId', (req,res) => {   // Essential
