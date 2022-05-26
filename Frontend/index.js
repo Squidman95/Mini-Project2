d1aaf6ec7e6483d9fec363ccabdc2edfb614482b
Squidman95 <squidmanse@gmail.com>
2022-05-26 09:55:55 +0200

    Cleaned up backend

--- Frontend/index.js
+++
@@ -1,3 +0,0 @@
-fetch("/")
-    .then(response => console.log(response))
-    .catch(reponse => alert("something went wrong"));
--- Frontend/src/Components/Topbar/Topbar.js
+++
@@ -1,5 +0,0 @@
-function loadNavBar() {
-    $.get("/src/Components/Topbar/Topbar.html", function (data) {
-        $("#nav-placeholder").replaceWith(data);
-      });
-}
